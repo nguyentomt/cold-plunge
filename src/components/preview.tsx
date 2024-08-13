8a373d968ac8c90d2c1ac9b7cdd952dc238452a6
Tom Nguyen <tomtnguyen88@gmail.com>
2024-08-13 14:01:59 -0700

    updates video for mobile and svg position

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -10,15 +10,17 @@ export default function Preview() {
       <div className="h-screen flex flex-col items-center">
         <div id="video-container" className="relative w-screen h-screen">
           <video
-            src="/lofilab.mp4"
-            autoPlay
+            width="100%"
             muted
+            autoPlay
             loop
             playsInline
             className="-z-1 w-[100vw] h-[100vh] opacity-70 inset-0 absolute object-cover "
-          />
+          >
+            <source src="/lofilab.mp4" type="video/mp4" />
+          </video>
           <div className="absolute w-screen">
-            <div className="flex flex-col gap-4 items-center">
+            <div className="flex flex-col gap-4 items-center pt-8">
               <Image
                 src="/metalabs-logo.svg"
                 width="420"
@@ -45,8 +47,8 @@ export default function Preview() {
           </div>
 
           <div className="flex w-screen h-screen items-end justify-center gap-4 p-4 text-sm">
-            <Link href="/terms"><div id="terms" className="z-10 opacity-30 hover:opacity-70 hover:cursor-pointer transition duration-300">Terms of Service</div></Link>
-            <Link href="/privacy"><div id="privacy" className="z-10 opacity-30 hover:opacity-70 hover:cursor-pointer transition duration-300">Privacy Policy</div></Link>
+            <Link href="/terms"><div id="terms" className="z-10 opacity-0 hover:opacity-70 hover:cursor-pointer transition duration-300">Terms of Service</div></Link>
+            <Link href="/privacy"><div id="privacy" className="z-10 opacity-0 hover:opacity-70 hover:cursor-pointer transition duration-300">Privacy Policy</div></Link>
           </div>
         </div>
 
